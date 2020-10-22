6240f5cdf908df53a2713e5ad679f8aa9a2cf6b5
Lejla Alibegovic <lejla.alibegovic@edu.fit.ba>
2020-10-22 23:28:44 +0200

    init

---
+++ Calculator/Calculator/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Calculator.Models;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CalculationModel model)
+        {
+            switch (model.Operations)
+            {
+                case Operation.Add:
+                    {
+                        model.Result = model.FirstNumber + model.SecondNumber;
+                        break;
+                    }
+                case Operation.Substract:
+                    {
+                        model.Result = model.FirstNumber - model.SecondNumber;
+                        break;
+                    }
+                case Operation.Multiply:
+                    {
+                        model.Result = model.FirstNumber * model.SecondNumber;
+                        break;
+                    }
+            }
+            return View(model);
+        }
+
+
+    }
+}
---
+++ Calculator/Calculator/Models/CalculationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    public class CalculationModel
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public long Result { get; set; } = 0;
+        public Operation Operations {get;set;}
+    }
+    public enum Operation
+    {
+        Add,
+        Substract,
+        Multiply
+    }
+}
